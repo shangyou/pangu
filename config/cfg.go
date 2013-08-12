fcd9f4f4882a2746c1e07b114c6a288ab97b3ed0
wsy <wangshangyou3000@163.com>
2013-08-12 09:45:59 +0800

    添加了常量配置

--- config/cfg.go
+++ config/cfg.go
@@ -1,1 +1,5 @@
 package config
+
+const (
+	LOGIN_FIRST_URL = "http://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=Z3V5dWV0ZnRiJTQwMTYzLmNvbQ%3D%3D&rsakt=mod&checkpin=1&client=ssologin.js(v1.4.5)&_="
+)
