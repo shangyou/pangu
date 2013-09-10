8b8f3000222f4637911ba3d932ac63b9c92ef161
wsy <wangshangyou3000@163.com>
2013-09-10 10:25:22 +0800

    工具

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,29 @@
 package utils
+
+import (
+	"io"
+)
+
+func ReadAll(reader io.ReadCloser) string {
+	var str string
+	for {
+		buf := make([]byte, 1024)
+		n, err := reader.Read(buf)
+
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+
+		if n == 0 {
+			break
+		}
+		str += string(buf)
+	}
+
+	return str
+}
+
+func Change(header interface{}) string {
+	data, _ := header.(string)
+	return data
+}
