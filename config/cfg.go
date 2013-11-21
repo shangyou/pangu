00a5f20a8d93b65284f65da6d4770be9dd488577
Shangyou <wangshangyou3000@163.com>
2013-11-21 14:57:36 +0800

    修改配置文件

--- config/cfg.go
+++ config/cfg.go
@@ -4,7 +4,9 @@ const (
 	LOGIN_FIRST_URL = "http://login.sina.com.cn/sso/prelogin.php?entry=weibo&callback=sinaSSOController.preloginCallBack&su=Z3V5dWV0ZnRiJTQwMTYzLmNvbQ%3D%3D&rsakt=mod&checkpin=1&client=ssologin.js(v1.4.5)&_="
 	LOGIN_USERNAME  = "iwangsy@gmail.com"
 	LOGIN_PASSWORD  = "wangshangyou3000"
-	GET_PWD_SHELL   = "/Users/Smile/project/java/weiboAuto/shell/ssologin.sh"
+	//GET_PWD_SHELL   = "/Users/Smile/project/java/weiboAuto/shell/ssologin.sh"
+	GET_PWD_SHELL   = "/home/wwwroot/go/src/github.com/pangu/command/ssologin.sh"
 
-	LOG_PATH = "/Users/Smile/project/go/openSource/src/pangu/log/"
+	//LOG_PATH = "/Users/Smile/project/go/openSource/src/pangu/log/"
+	LOG_PATH = "/home/wwwroot/go/src/github.com/pangu/log/"
 )
